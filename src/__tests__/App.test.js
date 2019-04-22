1d9010f262b83e955dc96dd46e954a453f2d5970
Sunil Sandhu <sunil.sandhu87@gmail.com>
2019-04-22 23:11:07 +0100

    Added enzyme, ezyme-adapter-react-16, react-test-renderer and created setupTests.js which is picked up by CRA by default. Updated App.test.js to use mount function from enzyme instead of default ReactDOM setup

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from '../App';
 
 it('renders without crashing', () => {
+  mount(<App/>)
+});
+
+
+/*
+it('renders without crashing', () => {
+  // this can be used instead 
+  // if you prefer CRAs default setup without enzyme
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+*/
