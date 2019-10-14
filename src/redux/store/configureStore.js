6675d36e28a41942634d25a4af20e7bb961ac541
Sunil Sandhu <sunil.sandhu87@gmail.com>
2019-10-14 22:44:10 +0100

    streamlined store

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers";
 
-export default function configureStore(preloadedState) {
-  return createStore(rootReducer, preloadedState);
+export default function configureStore() {
+  return createStore(rootReducer);
 }
